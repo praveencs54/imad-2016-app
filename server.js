ca5b1eb78f15c92f639da4f8234f5ab1c539fcb6
praveencs54 <praveencs54@gmail.com>
2016-10-21 15:50:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/page-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
-app.get('/page-1', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
